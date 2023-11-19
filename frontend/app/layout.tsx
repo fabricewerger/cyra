6c8c4e1008cc8226d1e33b109daf3e79eb7ed7da
Fabrice Werger <fabricewerger@icloud.com>
2023-11-19 19:56:42 +0100

    add lenis smooth scroll

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,15 +1,28 @@
-import '@/app/ui/global.css';
-import { inter } from '@/app/ui/fonts';
+'use client'
 
+import '@/app/ui/global.css'
+import { inter } from '@/app/ui/fonts'
+import Lenis from '@studio-freight/lenis'
+import { useEffect } from 'react'
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
+  useEffect(() => {
+    const lenis = new Lenis()
+
+    function raf(time: number) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+  })
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
-  );
+  )
 }
